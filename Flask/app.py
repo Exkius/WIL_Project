6f2b791908ef61af1ac4a838635f6b4cf18e3c25
Genevieve-ZIlo <100548489+Genevieve-ZIlo@users.noreply.github.com>
2023-09-22 16:15:57 +1000

    Updated app.py

--- Flask/app.py
+++ Flask/app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Sample top five important features (you'll replace this with your actual feature list)
+# Sample top five important features (replace this with actual feature list)
 top_5_features = ['Feature1', 'Feature2', 'Feature3', 'Feature4', 'Feature5']
 
-# Sample model prediction function (you'll replace this with your actual model)
+# Sample model prediction function (replace this with  actual model)
 def predict(input_data):
     # Replace this with your model prediction logic
     return {'prediction': 'Yes', 'confidence': 0.75}
@@ -17,7 +17,7 @@ def index():
         for feature in top_5_features:
             user_input[feature] = float(request.form.get(feature, 0.0))
 
-        # Call your machine learning model to make predictions
+        # Call machine learning model to make predictions
         prediction_result = predict(user_input)
 
         return render_template('dashboard.html', top_5_features=top_5_features, prediction_result=prediction_result)
